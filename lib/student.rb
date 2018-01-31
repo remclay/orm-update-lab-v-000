adbd6425a9792f7bbcde4e23de41b6bc69b8b4c5
Remmy Clay <remmy.lauren@gmail.com>
2018-01-31 21:02:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,12 +12,13 @@ class Student
   end
 
   def self.create_table
+    sql = <--SQL
     CREATE TABLE students (
     id INTEGER PRIMARY KEY
     name TEXT
     grade INTEGER
     )
-
+    SQL
     DB[:conn]
   end
 
