9479dccbc61c4ab1377aea537af66edd61dc8b8d
Remmy Clay <remmy.lauren@gmail.com>
2018-01-31 21:00:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,13 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  
+  attr_accessor :name, :grade
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
 
 end
